c8f5e782f9ea08d553411fca1785b9adf4427194
Andres Cardenas <65802352+DominicanCodes@users.noreply.github.com>
2022-11-12 23:40:22 -0500

    Change game to while loop. checkWin() logic changed to switch statement

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 let options = ['rock', 'paper', 'scissors'];
+let userChoice = setUp()
 
 function setUp() {
-    // return parseInt(prompt('Choose your weapon:' + displayOptions()))
-    return 1
+    let userChoice = parseInt(prompt('Choose your weapon:' + displayOptions())) 
+    if (userChoice < 0 || userChoice > options.length)
+        return 0
+    return userChoice
 }
 
 function displayOptions() {
@@ -18,10 +21,25 @@ function getRandomGuess() {
     return parseInt((Math.random() * options.length)+1)
 }
 
-let userChoice = setUp()
+function checkWin(userChoice, opChoice = getRandomGuess()) {
+    console.log('Your choice: ' + options[userChoice-1] + '' + userChoice)
+    console.log('Opponent choice: ' + options[opChoice-1] + '' + opChoice)
 
-console.log('Your choice: ' + userChoice)
+    switch (userChoice) {
+        case 1: 
+            return (opChoice !== 2) ? 'You Win!' : 'You Lose'
+        case 2:
+            return (opChoice !== 3) ? 'You Win!' : 'You Lose'
+        case 3:
+            return (opChoice !== 1) ? 'You Win!' : 'You Lose'
+        default:
+            return 'Draw'
+    }
+        
+}
 
-let opChoice = getRandomGuess()
-
-console.log('Opponent choice: ' + opChoice)
+while (userChoice !== 0) {
+    checkWin(userChoice)
+    console.log('********************************')
+    userChoice = setUp()
+}
