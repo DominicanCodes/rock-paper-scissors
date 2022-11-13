25e4e66c5a755d836414b85f48381783e8fe0775
Andres Cardenas <65802352+DominicanCodes@users.noreply.github.com>
2022-11-12 21:27:10 -0500

    add functions that display game start screen

--- game.js
+++ game.js
@@ -2,5 +2,18 @@ console.log('Hello World')
 
 let options = ['rock', 'paper', 'scissors'];
 
-for (opt=0;opt<options.length; opt++)
-    console.log(options[opt]);
+function setUp() {
+    console.log('Choose your weapon:'
+                + displayOptions())
+}
+
+function displayOptions() {
+    let output = '\n'
+
+    for (opt=0;opt<options.length; opt++)
+        output += (opt+1) + ': ' + options[opt] + '\n'
+
+        return output
+}
+
+setUp()
